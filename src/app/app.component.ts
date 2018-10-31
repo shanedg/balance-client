c719ff753eac30363e095e975b4b602f50ee5743
Shane Garrity <shanedgarrity@gmail.com>
2018-10-31 02:13:55 -0400

    rename value to amount for clarity, add simple form error messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { Transaction } from './transaction';
 
 const TRANSACTION_DATA: Transaction[] = [
-  {id: 1, description: 'Hydrogen', value: 1.0079, date: new Date().toLocaleDateString()},
-  {id: 2, description: 'Helium', value: 4.0026, date: new Date().toLocaleDateString()},
-  // {id: 3, description: 'Lithium', value: 6.941, date: new Date().toLocaleDateString()},
-  // {id: 4, description: 'Beryllium', value: 9.0122, date: new Date().toLocaleDateString()},
-  // {id: 5, description: 'Boron', value: 10.811, date: new Date().toLocaleDateString()},
-  // {id: 6, description: 'Carbon', value: 12.0107, date: new Date().toLocaleDateString()},
-  // {id: 7, description: 'Nitrogen', value: 14.0067, date: new Date().toLocaleDateString()},
-  // {id: 8, description: 'Oxygen', value: 15.9994, date: new Date().toLocaleDateString()},
-  // {id: 9, description: 'Fluorine', value: 18.9984, date: new Date().toLocaleDateString()},
-  // {id: 10, description: 'Neon', value: 20.1797, date: new Date().toLocaleDateString()},
+  {id: 1, description: 'Hydrogen', amount: 1.0079, date: new Date().toLocaleDateString()},
+  {id: 2, description: 'Helium', amount: 4.0026, date: new Date().toLocaleDateString()},
+  // {id: 3, description: 'Lithium', amount: 6.941, date: new Date().toLocaleDateString()},
+  // {id: 4, description: 'Beryllium', amount: 9.0122, date: new Date().toLocaleDateString()},
+  // {id: 5, description: 'Boron', amount: 10.811, date: new Date().toLocaleDateString()},
+  // {id: 6, description: 'Carbon', amount: 12.0107, date: new Date().toLocaleDateString()},
+  // {id: 7, description: 'Nitrogen', amount: 14.0067, date: new Date().toLocaleDateString()},
+  // {id: 8, description: 'Oxygen', amount: 15.9994, date: new Date().toLocaleDateString()},
+  // {id: 9, description: 'Fluorine', amount: 18.9984, date: new Date().toLocaleDateString()},
+  // {id: 10, description: 'Neon', amount: 20.1797, date: new Date().toLocaleDateString()},
 ];
 
 @Component({
