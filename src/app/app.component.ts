b8aa28a0253ed44250f5ed4406f03ba6312042e1
Shane Garrity <shanedgarrity@gmail.com>
2018-10-31 02:42:49 -0400

    move transactions data into a service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TransactionsService } from './transactions.service';
 import { Transaction } from './transaction';
-
-const TRANSACTION_DATA: Transaction[] = [
-  {id: 1, description: 'Hydrogen', amount: 1.0079, date: new Date().toLocaleDateString()},
-  {id: 2, description: 'Helium', amount: 4.0026, date: new Date().toLocaleDateString()},
-  // {id: 3, description: 'Lithium', amount: 6.941, date: new Date().toLocaleDateString()},
-  // {id: 4, description: 'Beryllium', amount: 9.0122, date: new Date().toLocaleDateString()},
-  // {id: 5, description: 'Boron', amount: 10.811, date: new Date().toLocaleDateString()},
-  // {id: 6, description: 'Carbon', amount: 12.0107, date: new Date().toLocaleDateString()},
-  // {id: 7, description: 'Nitrogen', amount: 14.0067, date: new Date().toLocaleDateString()},
-  // {id: 8, description: 'Oxygen', amount: 15.9994, date: new Date().toLocaleDateString()},
-  // {id: 9, description: 'Fluorine', amount: 18.9984, date: new Date().toLocaleDateString()},
-  // {id: 10, description: 'Neon', amount: 20.1797, date: new Date().toLocaleDateString()},
-];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'balance';
-  // TODO: connect this to persistent storage
-  transactions:Transaction[] = [].concat(TRANSACTION_DATA);
+  transactions:Transaction[];
 
+  constructor(private transactionsService: TransactionsService) { }
+
+  ngOnInit() {
+    this.transactions = [].concat(this.transactionsService.getTransactions());
+  }
+
+  // TODO: strategy around adding transactions via service
+  // and what triggers update to ui.
   receiveNewTransaction($event) {
     /*
      * Treat  `this.transactions` as immutable so that this change
