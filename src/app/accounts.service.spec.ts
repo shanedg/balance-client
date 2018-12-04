1c70a9977c6e7b7e9cafd46bb5325c6355349c78
Shane Garrity <shanedgarrity@gmail.com>
2018-12-04 01:01:41 -0500

    fix smoke tests for app component and new services

--- src/app/accounts.service.spec.ts
+++ src/app/accounts.service.spec.ts
@@ -1,9 +1,17 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientModule
+} from '@angular/common/http';
 
 import { AccountsService } from './accounts.service';
 
 describe('AccountsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule,
+    ],
+    declarations: []
+  }));
 
   it('should be created', () => {
     const service: AccountsService = TestBed.get(AccountsService);
