1f4dd69504c66d7405e61eb05acc8dcba89323a7
Shane Garrity <shanedgarrity@gmail.com>
2018-12-17 03:35:05 -0500

    experiment with a form group for each transaction

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -4,18 +4,36 @@ import {
   OnChanges,
   Output,
   Input,
-  SimpleChanges
+  SimpleChanges,
+  OnInit
 } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  NgForm
+} from '@angular/forms';
 
-import { Transaction } from '../app.types';
+import { EditRowComponent } from '../edit-row/edit-row.component';
+import {
+  Account,
+  Bucket,
+  Transaction
+} from '../app.types';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.css']
 })
-export class TransactionsComponent implements OnChanges {
+export class TransactionsComponent implements OnChanges, OnInit {
+
+  // Receive accounts list from parent.
+  @Input() accounts: Account[];
+
+  // Receive buckets list from parent.
+  @Input() buckets: Bucket[];
 
   // Transaction data to display, fetched in parent.
   @Input() transactions: Transaction[];
@@ -37,7 +55,77 @@ export class TransactionsComponent implements OnChanges {
     'bucket',
   ];
 
+  initialAddValues = [
+    {
+      // id: null,
+      name: null,
+      amount: null,
+      details: null,
+      due: null,
+      scheduled: null,
+      effective: null,
+      fromAccount: null,
+      toAccount: null,
+      bucket: null,
+    },
+  ];
+  singletonTableSource = new MatTableDataSource(this.initialAddValues);
+  singletonColumns: string[] = [
+    // 'id', // from cms, we don't want to create this ourselves
+    'name',
+    'amount',
+    'details',
+    'due',
+    'scheduled',
+    'effective',
+    'fromAccount',
+    'toAccount',
+    'bucket',
+  ];
+
   constructor() { }
+
+  ngOnInit() {
+    // TODO: absolve this hack, should probably a subscription to the
+    // endpoint observable...
+    setTimeout(() => {
+      for (let transaction of this.transactions) {
+        Object.assign(transaction, {formControl: new FormGroup({
+          name: new FormControl(null, {
+            validators: []
+          }),
+          amount: new FormControl(null, {
+            validators: [
+              Validators.pattern(/^-?\d+(\.?\d{1,2})?$/)
+            ]
+          }),
+          details: new FormControl(null, {
+            validators: []
+          }),
+          due: new FormControl(null, {
+            validators: []
+          }),
+          scheduled: new FormControl(null, {
+            validators: []
+          }),
+          effective: new FormControl(null, {
+            validators: []
+          }),
+          fromAccount: new FormControl(null, {
+            validators: []
+          }),
+          toAccount: new FormControl(null, {
+            validators: []
+          }),
+          bucket: new FormControl(null, {
+            validators: []
+          }),
+        }), tableSource: new MatTableDataSource([transaction])});
+      }
+
+    }, 500);
+
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     this.transactionTableSource.data = this.transactions;
@@ -53,6 +141,15 @@ export class TransactionsComponent implements OnChanges {
 
   onCellClick(cellData: any) {
     console.log('cell click:', cellData);
+  }
+
+  /*
+   * Receive new transaction from `transactionEditEvent`,
+   * pass thru to parent.
+   */
+  receiveEditTransaction($event) {
+    console.log('editing:', $event);
+    this.transactionUpdatedEvent.emit($event);
   }
 
 }
